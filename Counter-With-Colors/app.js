1e0b1988e94a03691ca226a57a22408b57a1154b
Muhammed Bahadir Kucuk <munisebahadir@gmail.com>
2020-06-18 16:53:09 +0200

    colors flow corrected

--- Counter-With-Colors/app.js
+++ Counter-With-Colors/app.js
@@ -1,6 +1,6 @@
 let count = 0;
 let interval = 1000 ;
-let a;
+let a; // To set and clear Interval
 
 const number = document.getElementById('number');
 const btns = document.querySelectorAll('.btn');
@@ -9,32 +9,40 @@ btns.forEach(function(btn) {
     btn.addEventListener('click', function(event) {
         const styles = event.currentTarget.classList;
         if(styles.contains('decrease')){
-
-            number.style.color = 'red';
             clearInterval(a);
             a = setInterval(() => {
                 count--;
                 number.textContent = count;
-            }, 1000);
+                checkColor();
+            }, interval);
         }
         
         else if(styles.contains('increase')){
-            number.style.color = 'green';
             clearInterval(a);
             a = setInterval(() => {
                 count++;
                 number.textContent = count;
-            }, 1000); 
+                checkColor();
+            }, interval); 
          }
 
         else {
-            number.style.color = 'blue';
             clearInterval(a);
             count = 0;
             number.textContent = count;
-         }   
+            checkColor();
+         }
     });
 });
 
-
-
+function checkColor() {
+    if(count < 0){
+        number.style.color = 'red';
+     }
+     else if(count > 0){
+        number.style.color = 'green';
+     }
+     else{
+        number.style.color = 'blue';
+    }
+}
